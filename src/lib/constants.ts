0d944fa22b0fa6ebf106ad368cda206b3dcff3fb
Th3X-Zohir <zohirrayhanweb@gmail.com>
2026-04-02 11:30:00 +0600

    feat: add application constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,42 @@
+export const MIN_PAYMENT_AMOUNT = 100
+export const MAX_PAYMENT_AMOUNT = 10000000
+export const DEFAULT_LINK_EXPIRY_DAYS = 30
+
+export const MIN_PAYOUT_AMOUNT = 500
+export const MAX_PAYOUT_AMOUNT = 5000000
+
+export const PLATFORM_FEE_PERCENT = 0.75
+export const AAMARPAY_FEE_PERCENT = 2
+
+export const SERVICE_CATEGORIES = {
+  WEB_DEVELOPMENT: 'Web Development',
+  GRAPHIC_DESIGN: 'Graphic Design',
+  DATA_ENTRY: 'Data Entry',
+  CONSULTING: 'Consulting',
+  COPYWRITING: 'Copywriting',
+  VIDEO_EDITING: 'Video Editing',
+  SOCIAL_MEDIA: 'Social Media',
+  TRANSLATION: 'Translation',
+  OTHER: 'Other',
+} as const
+
+export const DISPUTE_REASONS = {
+  CLIENT_NOT_PAID: 'Client did not pay',
+  SERVICE_NOT_DELIVERED: 'Service not delivered',
+  OVERCHARGED: 'Overcharged amount',
+  DUPLICATE_CHARGE: 'Duplicate charge',
+  UNAUTHORIZED_CHARGE: 'Unauthorized charge',
+  OTHER: 'Other reason',
+} as const
+
+export const PAYMENT_METHODS = {
+  BKASH: 'bKash',
+  NAGAD: 'Nagad',
+  BANK: 'Bank Transfer',
+  CARD: 'Card Payment',
+} as const
+
+export const PAYOUT_METHODS = {
+  BKASH: 'bKash',
+  BANK: 'Bank Transfer',
+} as const
